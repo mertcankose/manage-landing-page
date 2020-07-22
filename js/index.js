cecfb4b8014d65df5a4d164339b40355b3e2266d
mertcankose <mertcankose97@outlook.com>
2020-07-22 21:45:14 +0300

    third saved

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
-$(".header-switch").on('click',()=>{
-    $(".header").toggleClass("nav-show"); // we added the header because when we click switch button, header could have changed.
-    $("body").toggleClass("body-opac");
+const headerSwitch = document.querySelector('.header-switch');
+const header = document.querySelector('.header');
+const headerSwitchClose = document.querySelector('.header-switch-close');
+
+headerSwitch.addEventListener('click',()=>{
+    header.classList.toggle("nav-show");
+    headerSwitch.style.display = "none";
+    headerSwitchClose.style.display="flex";
+});
+
+headerSwitchClose.addEventListener('click',()=>{
+    header.classList.toggle("nav-show");
+    headerSwitch.style.display = "flex";
+    headerSwitchClose.style.display="none";
 });
